b818137a640298957a51de8cfcceb120fe4dcdc2
Mikhail Pavlov <redrik09@gmail.com>
2020-11-29 20:33:05 +0700

    Spring initializr

---
+++ src/main/java/com/github/Mrak2017/carpoketbook/monobackend/MonobackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.Mrak2017.carpoketbook.monobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonobackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonobackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/Mrak2017/carpoketbook/monobackend/MonobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Mrak2017.carpoketbook.monobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonobackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
