10d4b3882da997bae9d01a569c968fa1b48e531a
Mikhail Pavlov <redrik09@gmail.com>
2020-12-10 22:01:08 +0700

    temporary removing db

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# car-maintenance-pocketbook
+# Car Maintenance Pocketbook application
+![Deploy to Google Cloud](https://github.com/Mrak2017/car-maintenance-pocketbook/workflows/Create%20image%20and%20deploy%20to%20Google%20Cloud/badge.svg?branch=main)
--- monobackend/src/main/java/com/github/Mrak2017/carpoketbook/monobackend/entity/car/Car.java
+++ monobackend/src/main/java/com/github/Mrak2017/carpoketbook/monobackend/entity/car/Car.java
@@ -4,39 +4,38 @@ import com.github.Mrak2017.carpoketbook.monobackend.entity.core.BaseStoredEntity
 import com.github.Mrak2017.carpoketbook.monobackend.entity.owner.Person;
 import lombok.Data;
 
-import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Data
-@Entity
-@Table(name = "cp_car")
+//@Entity
+//@Table(name = "cp_car")
 public class Car extends BaseStoredEntity {
 
-    @Column(name = "government_number")
+//    @Column(name = "government_number")
     @Size(max = 6)
     @NotNull
     private String governmentNumber;
 
-    @Column(name = "region_code")
+//    @Column(name = "region_code")
     @Size(max = 3)
     @NotNull
     private Integer regionCode;
 
-    @Column
+//    @Column
     @NotNull
     private String color;
 
-    @Column
-    @Enumerated(EnumType.STRING)
+//    @Column
+//    @Enumerated(EnumType.STRING)
     @NotNull
     private CarModel model;
 
-    @Column(name = "creation_year")
+//    @Column(name = "creation_year")
     @NotNull
     private Integer creationYear;
 
-    @ManyToOne
-    @JoinColumn(name = "owner_id")
+//    @ManyToOne
+//    @JoinColumn(name = "owner_id")
     private Person owner;
 }
