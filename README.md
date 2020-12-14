a1c17c2058b3690a3b8ef2e3c805c68c42fa294b
Mikhail Pavlov <redrik09@gmail.com>
2020-12-14 21:19:15 +0700

    adding heroku db-connection

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Car Maintenance Pocketbook application
 ![Deploy to Google Cloud](https://github.com/Mrak2017/car-maintenance-pocketbook/workflows/Create%20image%20and%20deploy%20to%20Google%20Cloud/badge.svg?branch=main)
+
+[Application url (Google Cloud)](https://car-pocketbook-byh6ubypwq-ew.a.run.app/)
+
+DB hosted in [Heroku](https://www.heroku.com/)
--- monobackend/src/main/java/com/github/Mrak2017/carpoketbook/monobackend/entity/core/BaseStoredEntity.java
+++ monobackend/src/main/java/com/github/Mrak2017/carpoketbook/monobackend/entity/core/BaseStoredEntity.java
@@ -1,21 +1,24 @@
 package com.github.Mrak2017.carpoketbook.monobackend.entity.core;
 
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
-//@MappedSuperclass
+@MappedSuperclass
 public abstract class BaseStoredEntity {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-//    @Column
-//    @CreationTimestamp
+    @Column
+    @CreationTimestamp
     private LocalDateTime insertDate;
 
-//    @Column
-//    @UpdateTimestamp
+    @Column
+    @UpdateTimestamp
     private LocalDateTime updateDate;
 }
